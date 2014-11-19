68a174505ee6321d8776a91550818e5b5e3cadc2
Robin Thrift <robin@we-two-design.com>
2014-11-19 15:23:47 +0100

    added simple relationship

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ var fortune = require('fortune'),
 server.resource('user', {
     username: String,
     email: String,
-    age: Number
+    age: Number,
+    posts: [{ref: 'post', inverse: 'author'}]
+});
+
+server.resource('post', {
+    content: String,
+    title: String,
+    date: Date,
+    author: 'user'
 });
 
 server.listen(1337);
